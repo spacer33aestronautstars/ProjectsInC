f56ce72ee33074389fc59a04c37678ad5a84afc2
Yogesh yadav <145289183+spacer33aestronautstars@users.noreply.github.com>
2025-02-25 06:34:23 +0530

    Execute Escape sequence character in C

---
+++ BasicC/Escape.c
@@ -0,0 +1,13 @@
+//In this program we use Escape Sequence or Execution Characters of C Programming Language
+#include<stdio.h>
+#include <windows.h>
+int main(){
+    printf("C is the best programming language of that time isn't it \a. \nThere are so many other popular language in the world like\vJava\vC++\tPython.\n");
+    printf("\\Presents a character with backslash ");
+    printf(" \bMoves the cursor to the previous position of the current line\n");
+    printf("Moves the cursor to beginning of the current line.\r");
+    Beep(1000, 300);          // Windows API Beep
+    printf("Vertical Tab Test: Hello\vWorld\n"); // May not show effect
+    printf("New Line Test: Hello\nWorld\n");
+    return 0;
+}
